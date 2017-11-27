06e8bfb363d3892b32ebd9546021eab4e2ea0e34
Salvo Virga <salvo.virga@tum.de>
2017-11-27 02:22:42 +0100

    modified the publishers class to work with the new flatbuffers
    Former-commit-id: 28fb5f01f88daa43fb5cfb26a2eb25b4cea2d1ec

--- src/Publisher.cpp
+++ src/Publisher.cpp
@@ -1,49 +1,54 @@
-#include "Publisher.h"
+#include <iostream>
+
+#include "publisher.h"
+
+simple::Publisher::Publisher(const std::string& port, zmq::context_t& context)
+{
+  socket_ = std::make_unique<zmq::socket_t>(context, ZMQ_PUB);
+  try
+  {
+    socket_->bind(port);
+  }
+  catch (zmq::error_t& e)
+  {
+    std::cerr << "Error - Could not bind to the socket:" << e.what();
+  }
+}
+
+simple::Publisher::~Publisher()
+{
+  socket_->close();
+}
+
+void simple::Publisher::publish(const uint8_t* msg, const int size)
+{
+  // TODO: add message topic to allow subscription filter
+  const char* topic = flatbuffers::GetBufferIdentifier(msg);
+
+  zmq::message_t ZMQ_message(size);
+  memcpy(ZMQ_message.data(), msg, size);
+
+  try
+  {
+    socket_->send(ZMQ_message);
+  }
+  catch (zmq::error_t& e)
+  {
+    std::cerr << "Error - Could not send the message: " << e.what();
+  }
+}
+
+void simple::Publisher::publish(const simple_msgs::GenericMessage& msg)
+{
+  uint8_t* buffer = msg.getBufferData();
+  int buffer_size = msg.getBufferSize();
+  publish(buffer, buffer_size);
+}
 
 void simple::Publisher::publish(const flatbuffers::FlatBufferBuilder& msg)
 {
-	/// Sends the message by the open socket of the publisher. Any type of message
-	/// is supported (T is a flatbufferBuilder)
-
-	//get the data from the message
-	uint8_t* buf = msg.GetBufferPointer();
-
-	// add message topic to allow subscription filter TODO
-
-	const char* topic = flatbuffers::GetBufferIdentifier(buf);
-	
-
-	int buffersize = msg.GetSize();
-
-	zmq::message_t ZMQmsg(buffersize);
-
-	memcpy(ZMQmsg.data(), buf, buffersize);
-
-	try
-	{
-		socket->send(ZMQmsg);
-	}
-	catch (zmq::error_t& e)
-	{
-		std::cerr << "Could not send message: " << e.what();
-	}
-}
-simple::Publisher::Publisher(std::string port, zmq::context_t& context)
-{
-	/// Class constructor: opens a socket of type ZMQ_PUB and binds it to port
-
-	socket = std::make_unique<zmq::socket_t>(context, ZMQ_PUB);
-	try
-	{
-		socket->bind(port);
-	}
-	catch (zmq::error_t& e)
-	{
-		std::cerr << "could not bind to socket:" << e.what();
-	}
-}
-simple::Publisher::~Publisher()
-{
-	/// Class destructor: Closes the socket
-	socket->close();
+  // Get the data from the message.
+  uint8_t* buffer = msg.GetBufferPointer();
+  int buffer_size = msg.GetSize();
+  publish(buffer, buffer_size);
 }
