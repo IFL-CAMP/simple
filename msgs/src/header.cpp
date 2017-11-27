4bf32ee41ddf12a7ea8916dce165831f2c25765a
Salvo Virga <salvo.virga@tum.de>
2017-11-27 15:44:40 +0100

    MODIFIED: wrappers - added mutex to the modified flag MODIFIED: wrappers - calling Clear() on the buffer before creating another message
    Former-commit-id: 44f24947fe6659fb6a22f9a0bca7756587bbcec2

--- msgs/src/header.cpp
+++ msgs/src/header.cpp
@@ -10,8 +10,10 @@ simple_msgs::Header::Header(const uint8_t* data)
 }
 uint8_t* simple_msgs::Header::getBufferData() const
 {
+  std::lock_guard<std::mutex> lock(mutex_);
   if (field_mofified_)
   {
+    builder_->Clear();
     auto h = CreateHeaderFbs(*builder_, seq_n_, builder_->CreateString(frame_id_), timestamp_);
     builder_->Finish(h);
     field_mofified_ = false;
