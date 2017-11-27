8c0a6ccfaa59c54a438f5e247aa30d46ec46cef9
Salvo Virga <salvo.virga@tum.de>
2017-11-27 18:56:40 +0100

    cleaned up tests
    Former-commit-id: 3b44424f4e8b181037affa59898c9026aeda736a

--- tests/publisher/main.cpp
+++ tests/publisher/main.cpp
@@ -5,7 +5,7 @@
 #include <signal.h>
 #include <string>
 #include "header.h"
-#include "myContext.h"
+#include <thread>
 
 // handle interruptions
 static int s_interrupted = 0;
@@ -13,21 +13,16 @@ static void s_signal_handler(int signal_value)
 {
   s_interrupted = 1;
 }
+
 static void s_catch_signals()
 {
-  // struct signal action;
-  // action.sa_handler = s_signal_handler;
-  // action.sa_flags = 0;
-  // sigemptyset(&action.sa_mask);
   signal(SIGINT, s_signal_handler);
   signal(SIGTERM, s_signal_handler);
 }
 
 int main(int argc, char* argv[])
 {
-  // create the context (this makes sure the destructor of the context is the last one to be called, so the context will
-  // only be closed when the sockets are closed)
-  simple::myContext ctx;
+  using namespace std::chrono_literals;
 
   // create a header message, with wrapper
   int seq_n = 1;
@@ -36,24 +31,22 @@ int main(int argc, char* argv[])
   simple_msgs::Header h(seq_n, frame_id, timeStmp);
 
   // create a publisher
-  simple::Publisher pub("tcp://*:5555", *ctx.context);
+  simple::Publisher pub("tcp://*:5555");
 
   s_catch_signals();
-  int num = 3;
+
   while (!s_interrupted)
   {
     try
     {  // send the message continously
       pub.publish(h);
-      std::cout << "Header Message published"
-                << "\n";
+      std::cout << "Header Message published" << std::endl;
+      std::this_thread::sleep_for(1s);
     }
     catch (zmq::error_t& e)
     {
-      std::cout << "Something went wrong with the publishing..."
-                << "\n";
+      std::cout << "Something went wrong with the publishing..." << std::endl;
     }
-    //num--;
   }
 
   return 0;
--- tests/subscriber/main.cpp
+++ tests/subscriber/main.cpp
@@ -5,49 +5,41 @@
 #include "subscriber.h"
 #include "generic_message.h"
 #include "header.h"
-#include "myContext.h"
 
 static int s_interrupted = 0;
-static void s_signal_handler(int signal_value) { s_interrupted = 1; }
-
-static void s_catch_signals(void) {
-	signal(SIGINT, s_signal_handler);
-	signal(SIGTERM, s_signal_handler);
+static void s_signal_handler(int signal_value)
+{
+  s_interrupted = 1;
 }
 
-int main(int argc, char* argv[]) {
-
-	// create the context (this makes sure the destructor of the context is the last one to be called, so the context will
-	// only be closed when the sockets are closed)
-	simple::myContext ctx;
-
-	// create the subscriber
-
-	simple::Subscriber<simple_msgs::HeaderFbs> sub("tcp://localhost:5555",*ctx.context);
-
-	// create the holder for the incoming data
-	//simple_msgs::HeaderFbs h(0, "", 0.0);
-	
-	s_catch_signals();
-	while (!s_interrupted) {
-		try {
-			//receive message and print the content
-			auto h = sub.subscribe();
-			std::cout << "Received sequence number: " << h->sequence_number() << std::endl;
-			std::cout << "Received frame id: " << h->frame_id() << std::endl;
-			std::cout << "Received time stamp: " << h->timestamp()<< std::endl;
-
-		}
-
-		catch (zmq::error_t& e) {
-			std::cout << "Something went wrong with the subscription"
-				<< "\n";
-		}
-	}
-
-	std::cout << "Interruption received"
-		<< "\n";
-
-	return 0;
+static void s_catch_signals(void)
+{
+  signal(SIGINT, s_signal_handler);
+  signal(SIGTERM, s_signal_handler);
 }
 
+int main(int argc, char* argv[])
+{
+  // create the subscriber
+  simple::Subscriber<simple_msgs::HeaderFbs> sub("tcp://localhost:5555");
+
+  s_catch_signals();
+  while (!s_interrupted)
+  {
+    try
+    {
+      // receive message and print the content
+      auto h = sub.subscribe();
+      std::cout << "Received sequence number: " << h->sequence_number() << std::endl;
+      std::cout << "Received frame id: " << h->frame_id()->c_str() << std::endl;
+      std::cout << "Received time stamp: " << h->timestamp() << std::endl;
+    }
+
+    catch (zmq::error_t& e)
+    {
+      std::cout << "Something went wrong with the subscription" << std::endl;
+    }
+  }
+
+  std::cout << "Interruption received" << std::endl;
+}
