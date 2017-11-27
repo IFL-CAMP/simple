9150f9e94f91f1c49be21891780fd18c59af702c
Fernanda Langsch <fernanda.langsch@tum.de>
2017-11-27 09:32:05 +0100

    minor changes
    Former-commit-id: 4f2bd5710e6ab8d2e3baca27578eb13295b1ddba

--- include/Wrappers.h
+++ include/Wrappers.h
@@ -16,23 +16,31 @@ namespace simple_msgs{
 	public:
 		GenericMessage();
 		virtual ~GenericMessage();
-		virtual uint8_t* getBufferData();
-		virtual int getBufferSize();
+		virtual uint8_t* getBufferData()=0;
+		virtual int getBufferSize()=0;
 		
 	};
 
 	class HeaderW:GenericMessage{
+	public:
 		HeaderW();
 		HeaderW(const std::string& frame_id, const int seq, const double timeStamp);
 		HeaderW(const simple_msgs::header& headerFBS);
 		HeaderW(const uint8_t* data);
-
+		virtual uint8_t* getBufferData() override;
+		virtual int getBufferSize() override;
 	};
 	class ImageW:GenericMessage{
+	public:
 		ImageW();
-		ImageW(const uint8_t* data, const uint8_t* header, const double width, const double height, const double depth, const int resX, const int resY, const int resZ, const uint8_t* pose, const std::string& enconding);
+		ImageW(const std::vector<uint8_t>& data, const uint8_t* header, const double width, const double height, const double depth, const int resX, const int resY, const int resZ, const uint8_t* pose, const std::string& enconding);
+		ImageW(const std::vector<int>& data, const uint8_t* header, const double width, const double height, const double depth, const int resX, const int resY, const int resZ, const uint8_t* pose, const std::string& enconding);
+		ImageW(const std::vector<float>& data, const uint8_t* header, const double width, const double height, const double depth, const int resX, const int resY, const int resZ, const uint8_t* pose, const std::string& enconding);
+		ImageW(const std::vector<double>& data, const uint8_t* header, const double width, const double height, const double depth, const int resX, const int resY, const int resZ, const uint8_t* pose, const std::string& enconding);
 		ImageW(const simple_msgs::image& imageFBS);
 		ImageW(const uint8_t* data);
+		virtual uint8_t* getBufferData() override;
+		virtual int getBufferSize() override;
 	};
 
 
