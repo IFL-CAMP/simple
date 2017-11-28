06c14c1970bf1b320c0798336078f301b2edfb5f
Fernanda Langsch <fernanda.langsch@tum.de>
2017-11-28 18:10:43 +0100

    Image getDataUnionElem completed for all 4 types
    Former-commit-id: 8010aeffe51ecc1526b7af357c02d69f8f77fec9

--- msgs/src/image.cpp
+++ msgs/src/image.cpp
@@ -16,3 +16,12 @@ simple_msgs::data simple_msgs::Image<float>::getDataUnionType(){
 flatbuffers::Offset<void> simple_msgs::Image<uint8_t>::getDataUnionElem(){
 	return simple_msgs::CreatedataUInt8(*builder_, builder_->CreateVector(data_)).Union();
 }
+flatbuffers::Offset<void> simple_msgs::Image<int16_t>::getDataUnionElem(){
+	return simple_msgs::CreatedataInt16(*builder_, builder_->CreateVector(data_)).Union();
+}
+flatbuffers::Offset<void> simple_msgs::Image<float>::getDataUnionElem(){
+	return simple_msgs::CreatedataFloat(*builder_, builder_->CreateVector(data_)).Union();
+}
+flatbuffers::Offset<void> simple_msgs::Image<double>::getDataUnionElem(){
+	return simple_msgs::CreatedataDouble(*builder_, builder_->CreateVector(data_)).Union();
+}
