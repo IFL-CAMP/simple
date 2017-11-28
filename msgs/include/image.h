c06e811998371ca91f3f1997d9d1d900e8148098
Fernanda Langsch <fernanda.langsch@tum.de>
2017-11-28 09:44:12 +0100

    Template the image wrapper for the data type
    Former-commit-id: ae94f0be3eb1f5a06d99afe09f8c4036c0e2e499

--- msgs/include/image.h
+++ msgs/include/image.h
@@ -1,51 +1,92 @@
 #pragma once
 
+#include <mutex>
 #include "generic_message.h"
 #include "image_generated.h"
 
 namespace simple_msgs
 {
+template <typename T>
 class Image : public GenericMessage
-{public:
-	using GenericMessage::GenericMessage;
-	/**
-	* @brief TODO
-	* @param seq_n
-	* @param frame_id
-	* @param timestamp
-	*/
-	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<uint8_t>& data, const uint8_t* header, const uint8_t* origin);
-	/**
-	* @brief TODO
-	* @param seq_n
-	* @param frame_id
-	* @param timestamp
-	*/
-	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<double>& data, const uint8_t* header, const uint8_t* origin);
-	/**
-	* @brief TODO
-	* @param seq_n
-	* @param frame_id
-	* @param timestamp
-	*/
-	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<int16_t>& data, const uint8_t* header, const uint8_t* origin);
-	/**
-	* @brief TODO
-	* @param seq_n
-	* @param frame_id
-	* @param timestamp
-	*/
-	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<float>& data, const uint8_t* header, const uint8_t* origin){
-		flatbuffers::FlatBufferBuilder fbb;
-		auto dataVec = fbb.CreateVector(data);
-		auto offset = simple_msgs::CreatedataFloat(fbb, dataVec);
-	}
+{
+public:
+  using GenericMessage::GenericMessage;
+  /**
+   * @brief Wrapper for image data of type uint8.
+   * @param encoding
+   * @param resX
+   * @param resY
+   * @param resZ
+   * @param width Size of the image in the X direction
+   * @param height Size of the image in the Y direction
+   * @param depth Size of the image in the Z direction
+   * @param data Linearized vector of the image. Indexes follow the rule resX*resY*Z + resX*Y + X.
+   * @param headerBufPtr Pointer to the flatbuffer created for the header of the message.
+   * @param headerBufSize Size of the flatbuffer created for the message
+   * @param originBufPtr Pointer to the flatbuffer created for the origin of the image. Origin is of type Pose.
+   * @param originBufSize Size of the flatbuffer created for the origin of the image.
+   */
+  Image<T>(const std::string& encoding, const int resX, const int resY, const int resZ, const double width,
+        const double height, const double depth, const std::vector<T>& data, uint8_t* headerBufPtr,
+        int headerBufSize, uint8_t* originBufPtr, int originBufSize)
+  {
+    //flatbuffers::FlatBufferBuilder fbb;
+    // auto dataVec = fbb.CreateVector(data);
+    // auto offset = simple_msgs::CreatedataFloat(fbb, dataVec);
+  }
+  /**
+   * @brief TODO
+   * @param bufferPointer
+   */
+  Image(const uint8_t* bufferPointer);
+  /**
+   * @brief TODO
+   * @return
+   */
+  uint8_t* getBufferData() const;
+  /**
+   * @brief TODO
+   * @return
+   */
+  int getBufferSize() const;
+  /**
+   * @brief TODO
+   * @return
+   */
+  std::vector<int> getResolution();
+  /**
+   * @brief TODO
+   * @return
+   */
+  std::vector<double> getDimensions();
+  /**
+   * @brief TODO
+   * @return
+   */
 
+  /**
+   * @brief TODO
+   * @return
+   */
+
+  /**
+   * @brief TODO
+   * @return
+   */
+
+  /**
+   * @brief TODO
+   * @return
+   */
 
 private:
-	int resX_{ 0 }, resY_{ 0 }, resZ_{ 0 };
-	double width_{ 0.0 }, height_{ 0.0 }, depth_{ 0.0 };
-	std::string encoding_{ "" };
-
+  int resX_{ 0 }, resY_{ 0 }, resZ_{ 0 };
+  double width_{ 0.0 }, height_{ 0.0 }, depth_{ 0.0 };
+  std::string encoding_{ "" };
+  std::vector<T> data_{ {} };
+  uint8_t *headerBufPtr_{ nullptr }, originBufPtr_{ nullptr };
+  int headerBufSize_{ 0 }, originBufSize_{ 0 };
+  mutable bool field_mofified_{ false };
+  mutable std::mutex mutex_;
 };
-}
+}  // namespace simple_msgs
