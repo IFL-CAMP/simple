4613472b018b6dda66315f0a9647cef4e8f47e90
Fernanda Langsch <fernanda.langsch@tum.de>
2017-11-28 08:38:06 +0100

    Changed image Fbs definition. Started Image wrapper
    Former-commit-id: bc4e397e531f1b76f0368e329fab3d592c01b02d

--- msgs/include/image.h
+++ msgs/include/image.h
@@ -5,6 +5,42 @@
 namespace simple_msgs
 {
 class Image : public GenericMessage
-{
+{public:
+	using GenericMessage::GenericMessage;
+	/**
+	* @brief TODO
+	* @param seq_n
+	* @param frame_id
+	* @param timestamp
+	*/
+	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<uint8_t>& data, const uint8_t* header, const uint8_t* origin);
+	/**
+	* @brief TODO
+	* @param seq_n
+	* @param frame_id
+	* @param timestamp
+	*/
+	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<double>& data, const uint8_t* header, const uint8_t* origin);
+	/**
+	* @brief TODO
+	* @param seq_n
+	* @param frame_id
+	* @param timestamp
+	*/
+	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<int16_t>& data, const uint8_t* header, const uint8_t* origin);
+	/**
+	* @brief TODO
+	* @param seq_n
+	* @param frame_id
+	* @param timestamp
+	*/
+	Image(const std::string& encoding, const int resX, const int resY, const int resZ, const double width, const double height, const double depth, const std::vector<float>& data, const uint8_t* header, const uint8_t* origin);
+
+
+private:
+	int resX_{ 0 }, resY_{ 0 }, resZ_{ 0 };
+	double width_{ 0.0 }, height_{ 0.0 }, depth_{ 0.0 };
+	std::string encoding_{ "" };
+
 };
 }
