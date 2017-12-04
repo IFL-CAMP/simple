35dc9da4b4f56f63b9c1a6856495374146addc40
Fernanda Langsch <fernanda.langsch@tum.de>
2017-12-04 16:05:38 +0100

    Started Server class
    Former-commit-id: 4886458b9102d2b0839c9a4f951e8dc365e075fe

--- include/simple/server.h
+++ include/simple/server.h
@@ -10,19 +10,46 @@
 namespace simple
 {
 /**
- * @brief Creates a subscriber socket.
+ * @brief Creates a reply socket.
  */
 template <typename T>
 class Server
 {
 public:
-  Server(const std::string& port)
+  /**
+   * @brief Creates a reply socket. Opens a socket of type REP and connects it to the port. The user defined callback
+   * function is responsible for taking the received request and filling it with the reply data
+   */
+  Server(const std::string& port, const std::function<void(const T&)>& callback)
+    : socket_(std::make_unique<zmq::socket_t>(*context_, ZMQ_REP)), callback_(callback)
   {
+    try
+    {
+      socket_->bind(port);
+    }
+    catch (zmq::error_t& e)
+    {
+      std::cout << "could not bind socket:" << e.what();
+    }
+
+	// start thread of the server: wait for requests on a dedicated thread
+	t_ = std::thread(&Server::reply, this);
+
   }
 
-  ~Server();
+  ~Server(){
+	  // stop the reply loop
+	  alive_ = false;
 
-  void reply();
+	  // join thread
+	  t_.join();
+
+	  socket_->close();
+  }
+
+  void reply(){
+
+  }
 
 private:
   std::thread t_;
@@ -30,7 +57,7 @@ private:
   static std::unique_ptr<zmq::context_t, contextCloser> context_;
   std::unique_ptr<zmq::socket_t> socket_;  //<
 };
-
-std::unique_ptr<zmq::context_t, contextCloser> Server::context_(new zmq::context_t(1));
+template <typename T>
+std::unique_ptr<zmq::context_t, contextCloser> Server<T>::context_(new zmq::context_t(1));
 
 }  // namespace simple
