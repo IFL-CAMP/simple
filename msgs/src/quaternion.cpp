07521432fcbeb3fe73470d5d22086c8c3a296873
Salvo Virga <salvo.virga@tum.de>
2017-12-06 00:54:26 +0100

    quaternion wrapper done
    Former-commit-id: 2275eaf2dd5ba4a1afd54375381c1eae955eabef

--- msgs/src/quaternion.cpp
+++ msgs/src/quaternion.cpp
@@ -1,42 +1,94 @@
 
 #include "simple_msgs/quaternion.h"
 
-simple_msgs::Quaternion::Quaternion(const uint8_t* bufferPointer)
+namespace simple_msgs
+{
+Quaternion::Quaternion(const uint8_t* data)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  auto q = GetQuaternionFbs(bufferPointer);
+  auto q = GetQuaternionFbs(data);
   x_ = q->x();
   y_ = q->y();
   z_ = q->z();
   w_ = q->w();
-  mofified_ = true;
+  modified_ = true;
 }
 
-uint8_t* simple_msgs::Quaternion::getBufferData() const
+Quaternion& Quaternion::operator=(const Quaternion& q)
+{
+  if (this != std::addressof(q))
+  {
+    x_ = q.x_;
+    y_ = q.y_;
+    z_ = q.z_;
+    w_ = q.w_;
+    modified_ = true;
+  }
+  return *this;
+}
+
+bool Quaternion::operator==(const Quaternion& q) const
+{
+  return (x_ == q.x_ && y_ == q.y_ && z_ == q.z_ && w_ == q.w_);
+}
+
+bool Quaternion::operator!=(const Quaternion& q) const
+{
+  return !(*this == q);
+}
+
+uint8_t* Quaternion::getBufferData() const
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  if (mofified_)
+  if (modified_)
   {
     builder_->Clear();
-    simple_msgs::QuaternionFbsBuilder qBuilder(*builder_);
-    qBuilder.add_x(x_);
-    qBuilder.add_y(y_);
-    qBuilder.add_z(z_);
-    qBuilder.add_w(w_);
-    auto q = qBuilder.Finish();
-    simple_msgs::FinishQuaternionFbsBuffer(
-        *builder_, q);  // we have to explicitly call this method if we want the file_identifier to be set
-    mofified_ = false;
+    QuaternionFbsBuilder tmp_builder(*builder_);
+    tmp_builder.add_x(x_);
+    tmp_builder.add_y(y_);
+    tmp_builder.add_z(z_);
+    tmp_builder.add_w(w_);
+    FinishQuaternionFbsBuffer(*builder_, tmp_builder.Finish());
+    modified_ = false;
   }
   return builder_->GetBufferPointer();
 }
 
-void simple_msgs::Quaternion::setQuaternion(std::vector<double> quat)
+void Quaternion::fromVector(const std::array<double, 4>& q)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  x_ = quat.at(0);
-  y_ = quat.at(1);
-  z_ = quat.at(2);
-  w_ = quat.at(3);
-  mofified_ = false;
+  x_ = q[0];
+  y_ = q[1];
+  z_ = q[2];
+  w_ = q[3];
+  modified_ = true;
+}
+
+void Quaternion::setX(double x)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  x_ = x;
+  modified_ = true;
+}
+
+void Quaternion::setY(double y)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  y_ = y;
+  modified_ = true;
+}
+
+void Quaternion::setZ(double z)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  z_ = z;
+  modified_ = true;
+}
+
+void Quaternion::setW(double w)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  w_ = w;
+  modified_ = true;
+}
 }
