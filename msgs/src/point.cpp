bfbe4e97cd16e7532b4010d2010e977ad7fb47ce
Salvo Virga <salvo.virga@tum.de>
2017-12-06 00:01:06 +0100

    point wrapper done
    Former-commit-id: 0b6962c285855efefbe9ab23e1e9ce4679aee552

--- msgs/src/point.cpp
+++ msgs/src/point.cpp
@@ -1,39 +1,82 @@
 
 #include "simple_msgs/point.h"
 
-simple_msgs::Point::Point(const uint8_t* data)
+namespace simple_msgs
+{
+Point::Point(const uint8_t* data)
 {
   std::lock_guard<std::mutex> lock(mutex_);
   auto p = GetPointFbs(data);
   x_ = p->x();
   y_ = p->y();
   z_ = p->z();
-  mofified_ = true;
+  modified_ = true;
 }
 
-uint8_t* simple_msgs::Point::getBufferData() const
+Point& Point::operator=(const Point& p)
+{
+  if (this != std::addressof(p))
+  {
+    x_ = p.x_;
+    y_ = p.y_;
+    z_ = p.z_;
+    modified_ = true;
+  }
+  return *this;
+}
+
+bool Point::operator==(const Point& p) const
+{
+  return (x_ == p.x_ && y_ == p.y_ && z_ == p.z_);
+}
+
+bool Point::operator!=(const Point& p) const
+{
+  return !(*this == p);
+}
+
+uint8_t* Point::getBufferData() const
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  if (mofified_)
+  if (modified_)
   {
     builder_->Clear();
-    simple_msgs::PointFbsBuilder ptBuilder(*builder_);
+    PointFbsBuilder ptBuilder(*builder_);
     ptBuilder.add_x(x_);
     ptBuilder.add_y(y_);
     ptBuilder.add_z(z_);
-    auto p = ptBuilder.Finish();
-    FinishPointFbsBuffer(*builder_,
-                         p);  // we have to explicitly call this method if we want the file_identifier to be set
-    mofified_ = false;
+    FinishPointFbsBuffer(*builder_, ptBuilder.Finish());
+    modified_ = false;
   }
   return builder_->GetBufferPointer();
 }
 
-void simple_msgs::Point::setPoint(std::vector<double> pt)
+void Point::setPoint(const std::array<double, 3>& pt)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  x_ = pt.at(0);
-  y_ = pt.at(1);
-  z_ = pt.at(2);
-  mofified_ = true;
+  x_ = pt[0];
+  y_ = pt[1];
+  z_ = pt[2];
+  modified_ = true;
+}
+
+void Point::setX(const double x)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  x_ = x;
+  modified_ = true;
+}
+
+void Point::setY(const double y)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  y_ = y;
+  modified_ = true;
+}
+void Point::setZ(const double z)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  z_ = z;
+  modified_ = true;
+}
 }
