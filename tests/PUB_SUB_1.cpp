f276c98d6e36aaeda9f28750dacd1614c5f79bac
Unknown <fernanda.langsch@tum.de>
2017-12-06 12:47:39 +0100

    First test PUB-SUB with point written
    Former-commit-id: 50673185b8921369183966ba6344c920c1ddf55e

--- tests/PUB_SUB_1.cpp
+++ tests/PUB_SUB_1.cpp
@@ -1,3 +1,12 @@
+// Is noexcept supported?
+/*#if defined(__clang__) && __has_feature(cxx_noexcept) || \
+	defined(__GXX_EXPERIMENTAL_CXX0X__) && __GNUC__ * 10 + __GNUC_MINOR__ >= 46 || \
+	defined(_MSC_FULL_VER) && _MSC_FULL_VER >= 180021114
+#  define NOEXCEPT noexcept
+#else
+#  define NOEXCEPT
+#endif*/
+
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
@@ -19,33 +28,23 @@ void callbackFun(const simple_msgs::Point& pt){
 	recvP = pt;
 }
 
-//start a subscriber
-simple::Subscriber<simple_msgs::Point > sub("tcp://*:5555",callbackFun);
 
-//randomly generate the data to be sent
-srand(time(NULL));//start random seed
-double x = (double)rand()/RAND_MAX;
-double y = (double)rand() / RAND_MAX;
-double z = (double)rand() / RAND_MAX;
-simple_msgs::Point p(x, y, z);
 
 SCENARIO("PUB SUB POINT") {
 	GIVEN("An instance of a subscriber") {
+		//start a subscriber
+		simple::Subscriber<simple_msgs::Point > sub("tcp://*:5555", callbackFun);
 		WHEN("A publisher publishes data") {
 			simple::Publisher<simple_msgs::Point> pub("tcp://localhost:5555");
+			//randomly generate the data to be sent
+			srand(time(NULL));//start random seed
+			double x = (double)rand() / RAND_MAX;
+			double y = (double)rand() / RAND_MAX;
+			double z = (double)rand() / RAND_MAX;
+			simple_msgs::Point p(x, y, z);
+			pub.publish(p);
 			THEN("The data received is the same as the one sent") {
 				REQUIRE(p==recvP);
-			}
-		}
-		WHEN("A positive number is given") {
-			int positive_number = 22;
-			THEN("The sign is 1") {
-				REQUIRE(sign(positive_number) == 1);
-			}
-		}
-		WHEN("Zero is given") {
-			THEN("The sign is 0") {
-				REQUIRE(sign(0) == 0);
 			}
 		}
 	}
