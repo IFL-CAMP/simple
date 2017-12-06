1a4b7909ea83732c03a3889adcfc7a3b55e9d4be
Salvo Virga <salvo.virga@tum.de>
2017-12-07 00:26:07 +0100

    RotationMatrix wrapper done
    Former-commit-id: 251800e0ff852a2157aea0a58adb0d41e39f9918

--- msgs/src/rotation_matrix.cpp
+++ msgs/src/rotation_matrix.cpp
@@ -1,55 +1,109 @@
 
 #include "simple_msgs/rotation_matrix.h"
 
-void simple_msgs::RotationMatrix::setMatrixCoefs(std::vector<double> coefs)
+namespace simple_msgs
 {
-  r11_ = coefs.at(0);
-  r12_ = coefs.at(1);
-  r13_ = coefs.at(2);
-  r21_ = coefs.at(3);
-  r22_ = coefs.at(4);
-  r23_ = coefs.at(5);
-  r31_ = coefs.at(6);
-  r32_ = coefs.at(7);
-  r33_ = coefs.at(8);
+RotationMatrix::RotationMatrix(const uint8_t* data)
+{
+  auto r = GetRotationMatrixFbs(data);
+  matrix_ =
+      std::array<double, 9>{r->r11(), r->r12(), r->r13(), r->r21(), r->r22(), r->r23(), r->r31(), r->r32(), r->r33()};
   modified_ = true;
 }
 
-uint8_t* simple_msgs::RotationMatrix::getBufferData() const
+RotationMatrix& RotationMatrix::operator=(const RotationMatrix& m)
+{
+  if (this != std::addressof(m))
+  {
+    matrix_ = m.matrix_;
+    modified_ = true;
+  }
+  return *this;
+}
+
+bool RotationMatrix::operator==(const RotationMatrix& m) const
+{
+  return matrix_ == m.matrix_;
+}
+
+bool RotationMatrix::operator!=(const RotationMatrix& m) const
+{
+  return !(*this == m);
+}
+
+uint8_t* RotationMatrix::getBufferData() const
 {
   std::lock_guard<std::mutex> lock(mutex_);
   if (modified_)
   {
     builder_->Clear();
-    simple_msgs::RotationMatrixFbsBuilder rBuilder(*builder_);
-    rBuilder.add_r11(r11_);
-    rBuilder.add_r12(r12_);
-    rBuilder.add_r13(r13_);
-    rBuilder.add_r21(r21_);
-    rBuilder.add_r22(r22_);
-    rBuilder.add_r23(r23_);
-    rBuilder.add_r31(r31_);
-    rBuilder.add_r32(r32_);
-    rBuilder.add_r33(r33_);
-    auto r = rBuilder.Finish();
-    simple_msgs::FinishRotationMatrixFbsBuffer(
-        *builder_, r);  // we have to explicitly call this method if we want the file_identifier to be set
+    RotationMatrixFbsBuilder tmp_builder(*builder_);
+    tmp_builder.add_r11(matrix_[0]);
+    tmp_builder.add_r12(matrix_[1]);
+    tmp_builder.add_r13(matrix_[2]);
+    tmp_builder.add_r21(matrix_[3]);
+    tmp_builder.add_r22(matrix_[4]);
+    tmp_builder.add_r23(matrix_[5]);
+    tmp_builder.add_r31(matrix_[6]);
+    tmp_builder.add_r32(matrix_[7]);
+    tmp_builder.add_r33(matrix_[8]);
+    FinishRotationMatrixFbsBuffer(*builder_, tmp_builder.Finish());
     modified_ = false;
   }
   return builder_->GetBufferPointer();
 }
 
-simple_msgs::RotationMatrix::RotationMatrix(const uint8_t* bufferPointer)
+RotationMatrix RotationMatrix::getTranspose() const
 {
-  auto r = GetRotationMatrixFbs(bufferPointer);
-  r11_ = r->r11();
-  r12_ = r->r12();
-  r13_ = r->r13();
-  r21_ = r->r21();
-  r22_ = r->r22();
-  r23_ = r->r23();
-  r31_ = r->r31();
-  r32_ = r->r32();
-  r33_ = r->r33();
-  modified_ = true;
+  return RotationMatrix(matrix_[0], matrix_[3], matrix_[6], matrix_[1], matrix_[4], matrix_[7], matrix_[2], matrix_[5],
+                        matrix_[8]);
+}
+
+std::array<double, 3> RotationMatrix::getRow(int row_index) const
+{
+  if (0 <= row_index && row_index <= 2)
+  {
+    return std::array<double, 3>{matrix_[row_index * 3], matrix_[row_index * 3 + 1], matrix_[row_index * 3 + 2]};
+  }
+  else
+  {
+    throw std::out_of_range("Index out of range [0,2]");
+  }
+}
+
+std::array<double, 3> RotationMatrix::getColumn(int column_index) const
+{
+  if (0 <= column_index && column_index <= 2)
+  {
+    return std::array<double, 3>{matrix_[column_index], matrix_[3 * 1 + column_index], matrix_[3 * 2 + column_index]};
+  }
+  else
+  {
+    throw std::out_of_range("Index out of range [0,2]");
+  }
+}
+void RotationMatrix::setRow(int row_index, const std::array<double, 3>& values)
+{
+  for (auto i = 0; i < values.size(); ++i)
+  {
+    matrix_[row_index * 3 + i] = values[i];
+  }
+}
+
+void RotationMatrix::setColumn(int column_index, const std::array<double, 3>& values)
+{
+  for (auto i = 0; i < values.size(); ++i)
+  {
+    matrix_[3 * i + column_index] = values[i];
+  }
+}
+
+std::ostream& operator<<(std::ostream& out, const RotationMatrix& q)
+{
+  out << "RotationMatrix \n \t" << q.matrix_[0] << " " << q.matrix_[1] << " " << q.matrix_[2] << "\n"
+      << q.matrix_[3] << " " << q.matrix_[4] << " " << q.matrix_[5] << "\n"
+      << q.matrix_[6] << " " << q.matrix_[7] << " " << q.matrix_[8] << "\n";
+
+  return out;
+}
 }
