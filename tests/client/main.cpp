9263d6cac739877d4cf78337f20106cb938ab145
Salvo Virga <salvo.virga@tum.de>
2017-12-09 04:07:49 +0100

    modified tests for client and server
    Former-commit-id: ca53308a43c2ce01c6a76560d976735c09fa1698

--- tests/client/main.cpp
+++ tests/client/main.cpp
@@ -2,21 +2,8 @@
 
 #include <iostream>
 #include "simple/client.h"
-#include <signal.h>
 #include <string>
-#include "simple_msgs/header.h"
-#include <thread>
-#include "simple_msgs/image.h"
-#include "simple_msgs/pose.h"
-
-void dummy(const simple_msgs::Point& p)  // get the point and read the values inside
-{
-  std::cout << "Received point: " << std::endl;
-  auto vec = p.toVector();
-  std::cout << "new X: " << vec.at(0) << std::endl;
-  std::cout << "new Y: " << vec.at(1) << std::endl;
-  std::cout << "new Z: " << vec.at(2) << std::endl;
-}
+#include "simple_msgs/point.h"
 
 int main(int argc, char* argv[])
 {
@@ -24,23 +11,17 @@ int main(int argc, char* argv[])
   simple_msgs::Point p(5.0, 6.0, 7.0);
 
   // create a client
-  simple::Client<simple_msgs::Point> client("tcp://localhost:5555", dummy);
-  std::cout << "Request these cordinates: x=5.0, y=6.0, z=7.0" << std::endl;
-  int num = 10;
-  while (num > 0)
+  simple::Client<simple_msgs::Point> client("tcp://localhost:5555");
+  std::cout << "Send: x = 5.0, y = 6.0, z = 7.0. Expect: x = 6.0, y = 7.0, z = 8.0" << std::endl;
+
+  for (auto i = 0; i < 10; ++i)
   {
-    try
-    {  // send the request num times
-      client.request(p);
-      std::cout << "Point requested: " << num << std::endl;
-      std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-    catch (zmq::error_t& e)
-    {
-      std::cout << "Something went wrong with the request..." << std::endl;
-    }
-    num--;
+    std::cout << "Requesting point #" << i << "." << std::endl;
+    auto response = client.request(p);
+    std::cout << response << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
+
   std::cout << "Requesting ended" << std::endl;
   return 0;
 }
--- tests/server/main.cpp
+++ tests/server/main.cpp
@@ -9,20 +9,13 @@
 
 void dummy(simple_msgs::Point& p)  // get the point and add (1,1,1) to it
 {
-  std::cout << "Received point: " << std::endl;
-  auto vec = p.toVector();
-  vec.at(0) = vec.at(0) + 1.0;
-  vec.at(1) = vec.at(1) + 1.0;
-  vec.at(2) = vec.at(2) + 1.0;
-  std::cout << "new X: " << vec.at(0) << std::endl;
-  std::cout << "new Y: " << vec.at(1) << std::endl;
-  std::cout << "new Z: " << vec.at(2) << std::endl;
-  p.fromVector(vec);
+  std::cout << "Received point. " << std::endl;
+  p += 1.0;
 }
 
 int main(int argc, char* argv[])
 {
-  std::cout << "Creating server for Point" << std::endl;
+  std::cout << "Creating server for Point messages" << std::endl;
   simple::Server<simple_msgs::Point> server("tcp://*:5555", dummy);
 
   // wait for 25 seconds
