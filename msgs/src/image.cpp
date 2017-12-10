1bc74741469723af7f6a64db729f866fffee59ae
Salvo Virga <salvo.virga@tum.de>
2017-12-10 05:28:54 +0100

    WIP: image wrapper
    Former-commit-id: 5fb3dea6f628430b97afc2a8838143e9e771173c

--- msgs/src/image.cpp
+++ msgs/src/image.cpp
@@ -1,56 +1,53 @@
 
 #include "simple_msgs/image.h"
 
-namespace simple_msgs {
-
-  template <>
-  data Image<uint8_t>::getDataUnionType() const
-  {
-    return data_dataUInt8;
-  }
-
-  template <>
-  data Image<int16_t>::getDataUnionType() const
-  {
-    return data_dataInt16;
-  }
-
-  template <>
-  data Image<double>::getDataUnionType() const
-  {
-    return data_dataDouble;
-  }
-
-  template <>
-  data Image<float>::getDataUnionType() const
-  {
-    return data_dataFloat;
-  }
-
-  template <>
-  flatbuffers::Offset<void> Image<uint8_t>::getDataUnionElem() const
-  {
-    return CreatedataUInt8(*builder_, builder_->CreateVector(data_, dataLength_)).Union();
-  }
-
-  template <>
-  flatbuffers::Offset<void> Image<int16_t>::getDataUnionElem() const
-  {
-    return CreatedataInt16(*builder_, builder_->CreateVector(data_, dataLength_)).Union();
-  }
-
-  template <>
-  flatbuffers::Offset<void> Image<float>::getDataUnionElem() const
-  {
-    return CreatedataFloat(*builder_, builder_->CreateVector(data_, dataLength_)).Union();
-  }
-
-  template <>
-  flatbuffers::Offset<void> Image<double>::getDataUnionElem() const
-  {
-    return CreatedataDouble(*builder_, builder_->CreateVector(data_, dataLength_)).Union();
-  }
-
+namespace simple_msgs
+{
+template <>
+data Image<uint8_t>::getDataUnionType() const
+{
+  return data_uint8_type;
 }
 
+template <>
+data Image<int16_t>::getDataUnionType() const
+{
+  return data_int16_type;
+}
 
+template <>
+data Image<float>::getDataUnionType() const
+{
+  return data_float_type;
+}
+
+template <>
+data Image<double>::getDataUnionType() const
+{
+  return data_double_type;
+}
+
+template <>
+flatbuffers::Offset<void> Image<uint8_t>::getDataUnionElem() const
+{
+  return Createuint8_type(*builder_, builder_->CreateVector(data_, data_size_)).Union();
+}
+
+template <>
+flatbuffers::Offset<void> Image<int16_t>::getDataUnionElem() const
+{
+  return Createint16_type(*builder_, builder_->CreateVector(data_, data_size_)).Union();
+}
+
+template <>
+flatbuffers::Offset<void> Image<float>::getDataUnionElem() const
+{
+  return Createfloat_type(*builder_, builder_->CreateVector(data_, data_size_)).Union();
+}
+
+template <>
+flatbuffers::Offset<void> Image<double>::getDataUnionElem() const
+{
+  return Createdouble_type(*builder_, builder_->CreateVector(data_, data_size_)).Union();
+}
+}
