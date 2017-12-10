ddbf3aacbfbac07e343bc04374f9c785bd0d12b6
Salvo Virga <salvo.virga@tum.de>
2017-12-10 03:11:58 +0100

    revamped point wrapper
    Former-commit-id: 14a9227e45be40def810ee14f37c3bb54e398655

--- msgs/src/point.cpp
+++ msgs/src/point.cpp
@@ -3,36 +3,170 @@
 
 namespace simple_msgs
 {
-Point::Point(const uint8_t* data)
+Point::Point()
+  : GenericMessage()
 {
-  std::lock_guard<std::mutex> lock(mutex_);
-  auto p = GetPointFbs(data);
-  x_ = p->x();
-  y_ = p->y();
-  z_ = p->z();
-  modified_ = true;
 }
 
-Point& Point::operator=(const Point& p)
+Point::Point(double value)
+  : GenericMessage()
+  , data_{value, value, value}
 {
-  if (this != std::addressof(p))
+}
+
+Point::Point(double x, double y, double z)
+  : GenericMessage()
+  , data_{x, y, z}
+{
+}
+
+Point::Point(const std::array<double, 3>& array)
+  : GenericMessage()
+  , data_(array)
+{
+}
+
+Point::Point(std::array<double, 3>&& array)
+  : GenericMessage()
+  , data_(std::move(array))
+{
+}
+
+Point::Point(const uint8_t* data)
+  : GenericMessage()
+{
+  auto p = GetPointFbs(data);
+  data_[0] = p->x();
+  data_[1] = p->y();
+  data_[2] = p->z();
+}
+
+Point::Point(const Point& other)
+  : Point(other.data_)
+{
+}
+
+Point::Point(Point&& other)
+  : GenericMessage()
+  , data_(std::move(other.data_))
+{
+}
+
+Point& Point::operator=(const Point& other)
+{
+  if (this != std::addressof(other))
   {
-    x_ = p.x_;
-    y_ = p.y_;
-    z_ = p.z_;
+    std::lock_guard<std::mutex> lock(mutex_);
+    data_ = other.data_;
     modified_ = true;
   }
   return *this;
 }
 
-bool Point::operator==(const Point& p) const
+Point& Point::operator=(Point&& other)
 {
-  return (x_ == p.x_ && y_ == p.y_ && z_ == p.z_);
+  if (this != std::addressof(other))
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    data_ = std::move(other.data_);
+    modified_ = true;
+  }
+  return *this;
 }
 
-bool Point::operator!=(const Point& p) const
+Point& Point::operator=(const std::array<double, 3>& array)
 {
-  return !(*this == p);
+  std::lock_guard<std::mutex> lock(mutex_);
+  data_ = array;
+  modified_ = true;
+  return *this;
+}
+
+Point& Point::operator=(std::array<double, 3>&& array)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  data_ = std::move(array);
+  modified_ = true;
+  return *this;
+}
+
+Point& Point::operator=(const uint8_t* data)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  auto p = GetPointFbs(data);
+  data_[0] = p->x();
+  data_[1] = p->y();
+  data_[2] = p->z();
+  modified_ = true;
+
+  return *this;
+}
+
+bool Point::operator==(const Point& rhs) const
+{
+  return data_ == rhs.data_;
+}
+
+bool Point::operator!=(const Point& rhs) const
+{
+  return !(*this == rhs);
+}
+
+Point& Point::operator+=(const Point& rhs)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  std::transform(std::begin(data_), std::end(data_), std::begin(rhs.data_), std::begin(data_), std::plus<double>());
+  modified_ = true;
+  return *this;
+}
+
+Point operator+(Point lhs, const Point& rhs)
+{
+  lhs += rhs;
+  return lhs;
+}
+
+Point& Point::operator-=(const Point& rhs)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  std::transform(std::begin(data_), std::end(data_), std::begin(rhs.data_), std::begin(data_), std::minus<double>());
+  modified_ = true;
+  return *this;
+}
+
+Point operator-(Point lhs, const Point& rhs)
+{
+  lhs -= rhs;
+  return lhs;
+}
+
+Point& Point::operator*=(const Point& rhs)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  std::transform(std::begin(data_), std::end(data_), std::begin(rhs.data_), std::begin(data_),
+                 std::multiplies<double>());
+  modified_ = true;
+  return *this;
+}
+
+Point operator*(Point lhs, const Point& rhs)
+{
+  lhs *= rhs;
+  return lhs;
+}
+
+Point& Point::operator/=(const Point& rhs)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  std::transform(std::begin(data_), std::end(data_), std::begin(rhs.data_), std::begin(data_), std::divides<double>());
+  modified_ = true;
+  return *this;
+}
+
+Point operator/(Point lhs, const Point& rhs)
+{
+  lhs /= rhs;
+  return lhs;
 }
 
 uint8_t* Point::getBufferData() const
@@ -42,50 +176,41 @@ uint8_t* Point::getBufferData() const
   {
     builder_->Clear();
     PointFbsBuilder tmp_builder(*builder_);
-    tmp_builder.add_x(x_);
-    tmp_builder.add_y(y_);
-    tmp_builder.add_z(z_);
+    tmp_builder.add_x(data_[0]);
+    tmp_builder.add_y(data_[1]);
+    tmp_builder.add_z(data_[2]);
     FinishPointFbsBuffer(*builder_, tmp_builder.Finish());
     modified_ = false;
   }
   return builder_->GetBufferPointer();
 }
 
-void Point::fromVector(const std::array<double, 3>& p)
-{
-  std::lock_guard<std::mutex> lock(mutex_);
-  x_ = p[0];
-  y_ = p[1];
-  z_ = p[2];
-  modified_ = true;
-}
-
 void Point::setX(double x)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  x_ = x;
+  data_[0] = x;
   modified_ = true;
 }
 
 void Point::setY(double y)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  y_ = y;
+  data_[1] = y;
   modified_ = true;
 }
 void Point::setZ(double z)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  z_ = z;
+  data_[2] = z;
   modified_ = true;
 }
 
 std::ostream& operator<<(std::ostream& out, const Point& p)
 {
   out << "Point \n \t"
-      << "x: " << p.x_ << "\n \t"
-      << "y: " << p.y_ << "\n \t"
-      << "z: " << p.z_ << "\n";
+      << "x: " << p.data_[0] << "\n \t"
+      << "y: " << p.data_[1] << "\n \t"
+      << "z: " << p.data_[2] << "\n";
 
   return out;
 }
