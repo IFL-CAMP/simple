b1c676c22ef1912b8975c13467a535bb11429bfb
Salvo Virga <salvo.virga@tum.de>
2017-12-10 05:29:14 +0100

    changes to test image message pub/sub
    Former-commit-id: 67a886ef5b4866e5e8d1e9e497f45ddaef399c75

--- tests/subscriber/main.cpp
+++ tests/subscriber/main.cpp
@@ -3,16 +3,18 @@
 #include "simple/subscriber.hpp"
 #include "simple_msgs/header.h"
 #include "simple_msgs/point.h"
+#include "simple_msgs/image.h"
 
-void dummy_callback(const simple_msgs::Point& p)
+void dummy_callback(const simple_msgs::Image<uint8_t>& i)
 {
-  std::cout << p << std::endl;
+  auto h = i.getHeader();
+  std::cout << h << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
-  std::cout << "Creating a subscriber for Point messages." << std::endl;
-  simple::Subscriber<simple_msgs::Point> sub("tcp://localhost:5555", dummy_callback);
+  std::cout << "Creating a subscriber for Image messages." << std::endl;
+  simple::Subscriber<simple_msgs::Image<uint8_t>> sub("tcp://localhost:5555", dummy_callback);
 
   // wait for 5 seconds
   std::this_thread::sleep_for(std::chrono::seconds(20));
