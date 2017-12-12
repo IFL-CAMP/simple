f2a56a6a7c93b501b54131c73051113519bd7d15
Fernanda Langsch <fernanda.langsch@tum.de>
2017-12-12 11:28:21 +0100

    All messages published are received
    Former-commit-id: c5f4d1fcda1dbcc99f8a71d63e49625b0e7f6e08

--- tests/single_pub_sub.cpp
+++ tests/single_pub_sub.cpp
@@ -13,6 +13,7 @@
 // create static point for comparing with data sent
 simple_msgs::Point received_point(0.0, 0.0, 0.0);
 bool running = false;
+int numReceives = 0;
 
 simple_msgs::Point createRandomPoint()
 {
@@ -26,6 +27,7 @@ simple_msgs::Point createRandomPoint()
 void callbackFunction(const simple_msgs::Point& p)
 {
   received_point = p;
+  numReceives++;
   if (!running)
   {
     running = true;
@@ -37,26 +39,26 @@ SCENARIO("Publish and subscribe to a Point message.")
   GIVEN("An instance of a subscriber.")
   {
     // start a subscriber
+    simple::Publisher<simple_msgs::Point> pub("tcp://*:5555");
+    auto p = createRandomPoint();
+    pub.publish(p);
     simple::Subscriber<simple_msgs::Point> sub("tcp://localhost:5555", callbackFunction);
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     WHEN("A publisher publishes data")
     {
-      simple::Publisher<simple_msgs::Point> pub("tcp://*:5555");
-      std::this_thread::sleep_for(std::chrono::seconds(2));
-
       for (int i = 0; i < 10; ++i)
       {
         auto p = createRandomPoint();
         pub.publish(p);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
         if (running)
         {
-          THEN("The data received is the same as the one sent")
-          {
-            REQUIRE(p == received_point);
-          }
+          THEN("The data received is the same as the one sent") { REQUIRE(p == received_point); }
         }
       }
     }
+    REQUIRE(numReceives == 10);
   }
 }
