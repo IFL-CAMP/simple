3caaa11fbd37fbaa4fc73645ebe2f43a61724aae
Salvo Virga <salvo.virga@tum.de>
2018-01-17 11:41:21 +0100

    Not using a shared_ptr for the context anymore, this should all work fine
    Former-commit-id: 7ee8f4333f131e8a4a3fb35a197d041c01e43e91

--- include/simple/context_manager.hpp
+++ include/simple/context_manager.hpp
@@ -1,23 +1,17 @@
 #pragma once
 
 #include <zmq.h>
-#include <memory>
-
-void deleter(void* ctx)
-{
-  zmq_ctx_term(ctx);
-}
 
 class ContextManager
 {
 public:
   ContextManager()
-    : context_(zmq_ctx_new(), deleter)
+    : context_(zmq_ctx_new())
   {
   }
-  //~ContextManager() { zmq_ctx_term(context_); }
-  void* instance() const { return context_.get(); }
+  ~ContextManager() { zmq_ctx_term(context_); }
+  void* instance() const { return context_; }
 
 private:
-  std::shared_ptr<void> context_;
+  void * context_;
 };
