f697246717e0103b067d7a313312e195de415f54
Salvo Virga <salvo.virga@tum.de>
2018-05-05 17:40:09 +0200

    moved the wrong pub/sub test together with the other pub/sub tests

--- tests/test_pub_sub.cpp
+++ tests/test_pub_sub.cpp
@@ -309,3 +309,22 @@ SCENARIO("Publish and subscribe to a RotationMatrixStamped message.") {
     REQUIRE(num_received_rotation_matrix_stamped == 10);
   }
 }
+
+// Connecting a subscriber to a wrong type of publisher.
+SCENARIO("Publish a Pose and subscribe to a Point message") {
+  GIVEN("An instance of a subscriber") {
+    num_received_point = 0;  // Reset this variable;
+    simple::Publisher<simple_msgs::Pose> pub("tcp://*:5555");
+    simple::Subscriber<simple_msgs::Point> sub("tcp://localhost:5555", callbackFunctionConstPoint);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    WHEN("A publisher publishes data") {
+      for (int i = 0; i < 10; ++i) {
+        auto p = createRandomPose();
+        pub.publish(p);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+      }
+    }
+    REQUIRE(num_received_point == 0);
+  }
+}
