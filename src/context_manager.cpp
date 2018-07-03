778e7f816dd59dcdeca2ba10858e16264e08e882
Salvo Virga <salvo.virga@tum.de>
2018-07-03 17:12:00 +0200

    ContextManager should now be thread safe

--- src/context_manager.cpp
+++ src/context_manager.cpp
@@ -1,6 +1,4 @@
 #include <simple/context_manager.hpp>
 
-std::unique_ptr<std::atomic<void*>, decltype(&simple::ContextManager::deleteContext)> simple::ContextManager::context_{
-    nullptr};
-// std::atomic<void*> simple::ContextManager::context_{nullptr};
-// std::unique_ptr<simple::ContextManager> simple::ContextManager::manager_{nullptr};
+std::unique_ptr<simple::ContextManager::ZMQContext> simple::ContextManager::context_{nullptr};
+std::atomic<void*> simple::ContextManager::ZMQContext::context_{nullptr};
