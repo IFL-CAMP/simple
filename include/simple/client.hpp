c8caf3c6426a1f354d2cfdae3124a8d06c7201d8
Salvo Virga <salvo.virga@tum.de>
2018-07-23 00:30:58 +0200

    Client documentation

--- include/simple/client.hpp
+++ include/simple/client.hpp
@@ -25,8 +25,15 @@
 #include "simple/generic_socket.hpp"
 
 namespace simple {
+
 /**
- * @brief Creates a Client socket for a specific type of message.
+ * @class Client client.hpp.
+ * @brief The Client class creates a ZMQ socket of type ZMQ_REQ that can send requests to a Server object using the
+ * message of type T passed to its request() method.
+ * @tparam T The simple_msgs type to use.
+ *
+ * Implements the logic for a Client in the Client / Server paradigm.
+ * A Client can send requests to a Server using messages of types T and receive back an answer.
  */
 template <typename T>
 class Client {
@@ -34,11 +41,11 @@ public:
   Client() = default;
 
   /**
-   * @brief Default constructor for a socket of type Client.
-   * @param address address the client connects to, in the form: \<PROTOCOL\>://\<HOSTNAME\>:\<PORT\>. e.g
+   * @brief Creates a ZMQ_REQ socket and connects it to the given address.
+   * @param [in] address - address the client connects to, in the form: \<PROTOCOL\>://\<HOSTNAME\>:\<PORT\>. e.g
    * tcp://localhost:5555.
-   * @param timeout Time, in msec, the client shall wait for a reply. Default 30 seconds.
-   * @param linger Time, in msec, unsent messages linger in memory after socket is closed. Default -1 (infinite).
+   * @param [in] timeout - Time, in msec, the client shall wait for a reply. Default 30 seconds.
+   * @param [in] linger - Time, in msec, unsent messages linger in memory after socket is closed. Default -1 (infinite).
    */
   explicit Client(const std::string& address, int timeout = 30000, int linger = -1)
     : socket_{ZMQ_REQ}, address_{address}, timeout_{timeout}, linger_{linger} {
@@ -55,11 +62,12 @@ public:
 
   /**
    * @brief Sends the request to a server and waits for an answer.
-   * @param msg: SIMPLE class wrapper for Flatbuffer messages.
+   * @param [in,out] msg - simple_msgs class wrapper for Flatbuffer messages..
    */
   bool request(T& msg) { return request(msg.getBufferData(), msg); }
 
 private:
+  // Initialize the client, setting up the socket and its configuration.
   void initClient() {
     if (!socket_.isValid()) { socket_.initSocket(ZMQ_REQ); }
     socket_.setTimeout(timeout_);
@@ -70,13 +78,14 @@ private:
   bool request(const std::shared_ptr<flatbuffers::DetachedBuffer>& buffer, T& msg) {
     bool success{false};
 
+    // Send the message to the Server and receive back the response.
     if (socket_.sendMsg(buffer, "[SIMPLE Client] - ") != -1) {
       if (socket_.receiveMsg(msg, "[SIMPLE Client] - ") != -1) {
         success = true;
 
       } else {
         std::cerr << "[SIMPLE Client] - No reply received. Aborting this request." << std::endl;
-        // Delete the existing socket and create a new one.
+        // If no message was received back we need to delete the existing socket and create a new one.
         socket_.closeSocket();
         initClient();
       }
@@ -84,10 +93,10 @@ private:
     return success;
   }
 
-  GenericSocket<T> socket_{};
-  std::string address_{""};
-  int timeout_{30000};
-  int linger_{-1};
+  GenericSocket<T> socket_{};  //< The internal socket.
+  std::string address_{""};    //< The address the Client is connected to.
+  int timeout_{30000};         //< Milliseconds the Client should wait for a reply from a Server.
+  int linger_{-1};             //< Milliseconds the messages linger in memory after the socket is closed.
 };
 }  // Namespace simple.
 
