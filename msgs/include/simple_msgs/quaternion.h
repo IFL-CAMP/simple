f5bdf01a763306c26be2b7ee01241fe51533ec85
Salvo Virga <salvo.virga@tum.de>
2018-08-29 00:25:59 +0200

    Quaternion documentation

--- msgs/include/simple_msgs/quaternion.h
+++ msgs/include/simple_msgs/quaternion.h
@@ -20,33 +20,91 @@
 #define SIMPLE_MSGS_QUATERNION_H
 
 #include <array>
-#include <iostream>
+#include <ostream>
 
 #include "generated/quaternion_generated.h"
 #include "generic_message.h"
 
 namespace simple_msgs {
 /**
- * @brief Quaternion: wrapper class around the QuaternionFbs generated code from flatbuffers.
+ * @class Quaternion quaternion.h.
+ * @brief Wrapper for a Flatbuffers Quaternion message.
  */
 class Quaternion : public GenericMessage {
 public:
   Quaternion() = default;
+
+  /**
+   * @brief Construct a Point message using the given parameters.
+   * Convention: x, y, z, w.
+   */
   Quaternion(double, double, double, double);
+
+  /**
+   * @brief Copy construct a Point message using the given 4D array.
+   * Convention: x, y, z, w.
+   */
   Quaternion(const std::array<double, 4>&);
+
+  /**
+   * @brief Move construct a Point message using the given 4D array.
+   * Convention: x, y, z, w.
+   */
   Quaternion(std::array<double, 4>&&) noexcept;
+
+  /**
+   * @brief Construct a Quaternion message using a raw memory coming from network.
+   */
   Quaternion(const void*);
+
+  /**
+   * @brief Copy constructor.
+   */
   Quaternion(const Quaternion&);
+
+  /**
+   * @brief Move constructor.
+   */
   Quaternion(Quaternion&&) noexcept;
 
+  /**
+   * @brief Copy assignment operator.
+   */
   Quaternion& operator=(const Quaternion&);
+
+  /**
+   * @brief Move assignment operator.
+   */
   Quaternion& operator=(Quaternion&&) noexcept;
+
+  /**
+   * @brief Copy assignment operator that uses raw memory coming from the network.
+   */
   Quaternion& operator=(std::shared_ptr<void*>);
+
+  /**
+   * @brief Copy assignment from a 4D array.
+   */
   Quaternion& operator=(const std::array<double, 4>&);
+
+  /**
+   * @brief Move assignment from a 4D array.
+   */
   Quaternion& operator=(std::array<double, 4>&&) noexcept;
 
+  /**
+   * @brief Returns true if lhs is equal to rhs, false otherwise.
+   */
   inline bool operator==(const Quaternion& rhs) const { return data_ == rhs.data_; }
+
+  /**
+   * @brief Returns true if lhs is not equal to rhs, false otherwise.
+   */
   inline bool operator!=(const Quaternion& rhs) const { return !(*this == rhs); }
+
+  /**
+   * @brief Stream extraction operator.
+   */
   friend std::ostream& operator<<(std::ostream&, const Quaternion&);
 
   /**
@@ -56,25 +114,29 @@ public:
 
   /**
    * @brief Returns the quaternion as an array for 4 elements.
-   * @return
    */
   std::array<double, 4> toVector() const { return data_; }
+
   /**
    * @brief Returns of the x quaternion component.
    */
   double getX() const { return data_[0]; }
+
   /**
    * @brief Returns of the y quaternion component.
    */
   double getY() const { return data_[1]; }
+
   /**
    * @brief Returns of the z quaternion component.
    */
   double getZ() const { return data_[2]; }
+
   /**
    * @brief Returns of the w quaternion component.
    */
   double getW() const { return data_[3]; }
+
   /**
    * @brief Modifies the x component of the quaternion.
    */
@@ -101,7 +163,7 @@ public:
   static inline std::string getTopic() { return QuaternionFbsIdentifier(); }
 
 private:
-  std::array<double, 4> data_{{0, 0, 0, 1}};
+  std::array<double, 4> data_{{0, 0, 0, 1}};  //! Internal array.
 };
 }  // Namespace simple_msgs.
 
