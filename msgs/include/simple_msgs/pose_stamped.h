ec9e751bcb657a92701e945106aa8a9bd5d3cca4
Salvo Virga <salvo.virga@tum.de>
2018-08-29 00:26:32 +0200

    PoseStamped documentation

--- msgs/include/simple_msgs/pose_stamped.h
+++ msgs/include/simple_msgs/pose_stamped.h
@@ -19,33 +19,79 @@
 #ifndef SIMPLE_MSGS_POSE_STAMPED_H
 #define SIMPLE_MSGS_POSE_STAMPED_H
 
-#include <iostream>
+#include <ostream>
 
 #include "generated/pose_stamped_generated.h"
 #include "header.h"
 #include "pose.h"
 
 namespace simple_msgs {
+/**
+ * @class PoseStamped pose_stamped.h.
+ * @brief Wrapper for a Flatbuffers PoseStamped message.
+ * It contains a Pose and a Header message.
+ */
 class PoseStamped : public GenericMessage {
 public:
   PoseStamped() = default;
+
+  /**
+   * @brief Construct a PoseStamped message given its Header and Pose.
+   */
   PoseStamped(const Header&, const Pose&);
+
+  /**
+   * @brief Construct a PoseStamped message given its Header and Pose.
+   */
   PoseStamped(Header&&, Pose&&);
+
+  /**
+   * @brief Construct a PoseStamped message using a raw memory coming from network.
+   */
   PoseStamped(const void*);
+
+  /**
+   * @brief Copy constructor.
+   */
   PoseStamped(const PoseStamped&);
+
+  /**
+   * @brief Move constructor.
+   */
   PoseStamped(PoseStamped&&) noexcept;
 
+  /**
+   * @brief Copy assignment operator.
+   */
   PoseStamped& operator=(const PoseStamped&);
+
+  /**
+   * @brief Move assignment operator.
+   */
   PoseStamped& operator=(PoseStamped&&) noexcept;
+
+  /**
+   * @brief Copy assignment operator that uses raw memory coming from the network.
+   */
   PoseStamped& operator=(std::shared_ptr<void*>);
 
+  /**
+   * @brief Returns true if lhs is equal to rhs, false otherwise.
+   */
   inline bool operator==(const PoseStamped& rhs) const { return (pose_ == rhs.pose_ && header_ == rhs.header_); }
+
+  /**
+   * @brief Returns true if lhs is not equal to rhs, false otherwise.
+   */
   inline bool operator!=(const PoseStamped& rhs) const { return !(*this == rhs); }
+
+  /**
+   * @brief Stream extraction operator.
+   */
   friend std::ostream& operator<<(std::ostream&, const PoseStamped&);
 
   /**
    * @brief Builds and returns the buffer accordingly to the values currently stored.
-   * @return the buffer data.
    */
   std::shared_ptr<flatbuffers::DetachedBuffer> getBufferData() const override;
 
@@ -53,12 +99,20 @@ public:
    * @brief Returns message Header.
    */
   inline Header& getHeader() { return header_; }
+
+  /**
+   * @brief Returns message Header.
+   */
   inline const Header& getHeader() const { return header_; }
 
   /**
    * @brief Returns the message Pose.
    */
   inline Pose& getPose() { return pose_; }
+
+  /**
+   * @brief Returns the message Pose.
+   */
   inline const Pose& getPose() const { return pose_; }
 
   /**
