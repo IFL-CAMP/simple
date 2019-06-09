16b5570a9c1abbe6c174ad88f58a2a54c05ecba4
Salvo Virga <salvo.virga@tum.de>
2019-06-09 18:17:42 +0200

    moved usage of flatbuffers to the implementation only

--- msgs/include/simple_msgs/generic_message.hpp
+++ msgs/include/simple_msgs/generic_message.hpp
@@ -11,7 +11,16 @@
 #ifndef SIMPLE_MSGS_GENERIC_MESSAGE_H
 #define SIMPLE_MSGS_GENERIC_MESSAGE_H
 
-#include <flatbuffers/flatbuffers.h>
+#include <memory>
+
+// Forward declarations.
+namespace flatbuffers {
+class DetachedBuffer;
+}
+
+namespace simple {
+class GenericSocket;
+}  // namespace simple
 
 namespace simple_msgs {
 /**
@@ -19,16 +28,19 @@ namespace simple_msgs {
  * @brief Base class for simple_msgs wrappers around Flatbuffers messages.
  */
 class GenericMessage {
-public:
-  GenericMessage() = default;
+protected:
+  GenericMessage();
   virtual ~GenericMessage() = default;
 
+  // The simple::GenericSocket class is a friend of this base class, so that it has access to the protected methods.
+  friend class simple::GenericSocket;
   /**
-   * @brief Assignment operator from a shared void*
+   * @brief Internal assignment operator from a shared void*.
    *
    * Every message has to be constructable from a void*, since that is what we get from ZMQ.
+   * This contructor is not available to users, it is only used internally.
    *
-   * @param [in] rhs - the pointer from which construct the message
+   * @param [in] rhs - the pointer from which construct the message.
    * @return
    */
   virtual GenericMessage& operator=(std::shared_ptr<void*> rhs) = 0;
