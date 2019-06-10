2c674fd538ce0f3642d592ee0b0a78910f925278
Salvo Virga <salvo.virga@tum.de>
2019-06-10 03:01:19 +0200

    [CMake] - remove dependencies from the config

--- simpleConfig.cmake
+++ simpleConfig.cmake
@@ -1,7 +1,4 @@
 # - Config file for the simple package
-include(CMakeFindDependencyMacro)
-find_dependency(ZeroMQ)
-find_dependency(Flatbuffers)
 include("${CMAKE_CURRENT_LIST_DIR}/simpleTargets.cmake")
 
 
